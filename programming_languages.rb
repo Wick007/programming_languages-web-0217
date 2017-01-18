431ec3a05b3e859c0ca4159390235c2efede70a7
Andrew Wick <a.wick17@gmail.com>
2017-01-18 14:10:58 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |styles, types|
+    types.each do |name, atr|
+      if new_hash.include? name
+      else
+        new_hash[name] = {}
+        new_hash[name][:style] = []
+      end
+      atr.each do |k, v|
+        new_hash[name][k] = v
+        new_hash[name][:style] << styles
+      end
+    end
+  end
+  return new_hash
 end
